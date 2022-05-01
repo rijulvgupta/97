0bb023ccaf67f89718a59a17da31139306c54f62
rijulvgupta <80291914+rijulvgupta@users.noreply.github.com>
2022-05-01 19:10:26 -0400

    Update guessinggame.py

--- guessinggame.py
+++ guessinggame.py
@@ -3,22 +3,15 @@ print("number guessing game")
 number=random.randit(1, 9)
 chances=0
 print (" guess a number between (1 and 9)")
-
 while chances <5:
-
   guess = int(input("enter your guess:-"))
-
   if guess == number:
     print (" Congrats You Won!!")
     break
-
   elif guess< number:
     print (" your guess was too low, guess a number higher than", guess)
-
   else:
     print (" your guess was too high, guess a number lower than", guess)
-
 chances+ = 1
-
 if chances >5:
   print(" You Lose, the number is", number)
