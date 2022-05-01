6de6dd93e0bf0c8c856c13eb94cf280a12fc5bd9
rijulvgupta <80291914+rijulvgupta@users.noreply.github.com>
2022-05-01 19:08:49 -0400

    Update guessinggame.py

--- guessinggame.py
+++ guessinggame.py
@@ -6,17 +6,17 @@ print (" guess a number between (1 and 9)")
 
 while chances <5:
 
-guess = int(imput("enter your guess:-"))
+  guess = int(input("enter your guess:-"))
 
-if guess == number:
-  print (" Congrats You Won!!")
-  break
+  if guess == number:
+    print (" Congrats You Won!!")
+    break
 
-elif guess< number:
-  print (" your guess was too low, guess a number higher than", guess)
+  elif guess< number:
+    print (" your guess was too low, guess a number higher than", guess)
 
-else:
-  print (" your guess was too high, guess a number lower than", guess)
+  else:
+    print (" your guess was too high, guess a number lower than", guess)
 
 chances+ = 1
 
